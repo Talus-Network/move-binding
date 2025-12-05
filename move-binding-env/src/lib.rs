5e890d74461b3169217559b82fa81363863a68eb
David <davidrotari9@gmail.com>
2025-12-05 16:18:13 +0100

    Initial commit

---
+++ move-binding-env/src/lib.rs
@@ -0,0 +1,10 @@
+//! Session helper wrapping sui-rpc and sui-transaction-builder.
+
+pub struct SuiEnv;
+
+impl SuiEnv {
+    /// Placeholder connect; to be implemented.
+    pub fn new() -> Self {
+        Self
+    }
+}
